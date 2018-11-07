a4b8e531d325520bfc8640a8383fe609f8061258
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-11-07 23:39:50 +0100

    Fix Flake8  violations

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,16 +10,19 @@ def test_kubelet_is_installed(host):
     kubelet = host.package('kubelet')
 
     assert kubelet.is_installed
-    
+
+
 def test_kubeadm_is_installed(host):
     kubeadm = host.package('kubeadm')
 
     assert kubeadm.is_installed
 
+
 def test_kubectl_is_installed(host):
     kubectl = host.package('kubectl')
 
     assert kubectl.is_installed
+
 
 def test_docker_is_installed(host):
     docker = host.package('docker')
