9c0233f8fba50f7baa3907effd67d82cb8cd7b66
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-01-18 16:34:54 +0100

    Update no-role test

--- molecule/no-role/tests/test_default.py
+++ molecule/no-role/tests/test_default.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -7,14 +6,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# These packages must NOT be installed
-@pytest.mark.parametrize('name', [
-  ('kubelet', '1.12.1'),
-  ('kubeadm', '1.12.1'),
-  ('kubectl', '1.12.1'),
-  ('docker-ce', '18.06.0')
-])
-def test_package_is_installed(host, name, version):
-    package = host.package(name)
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
 
-    assert package.is_installed
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
